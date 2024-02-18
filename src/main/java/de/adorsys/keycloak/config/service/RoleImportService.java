618ba85f32ccc02c948ea09048b8a2f9cc977a62
odafna <ori.dafna@cognyte.com>
2024-02-18 16:54:38 +0200

    add import

--- src/main/java/de/adorsys/keycloak/config/service/RoleImportService.java
+++ src/main/java/de/adorsys/keycloak/config/service/RoleImportService.java
@@ -35,6 +35,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
 import java.util.Map;
